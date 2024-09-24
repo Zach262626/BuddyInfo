7295c9b9fc512ec04ba2394d8dd125897287be1b
zachg <zachgallant26@gmail.com>
2024-09-23 20:03:39 -0400

    added a new buddy

--- src/AdressBook.java
+++ src/AdressBook.java
@@ -6,7 +6,7 @@ public class AdressBook {
     public AdressBook() {
         buddies = new ArrayList<BuddyInfo>();
         this.buddies.add(new BuddyInfo("Joe", "50 Blue ave, Ottawa, ON", 5067891111L));
-        this.buddies.add(new BuddyInfo("Zachddd", "765 Orange ave, Ottawa, ON", 5067891112L));
+        this.buddies.add(new BuddyInfo("Zach", "765 Orange ave, Ottawa, ON", 5067891112L));
         this.buddies.add(new BuddyInfo("Tom", "2 Pink ave, Ottawa, ON", 5067891113L));
         this.buddies.add(new BuddyInfo("Amelia", "345 Black ave, Ottawa, ON", 5067891114L));
     }
@@ -22,9 +22,11 @@ public class AdressBook {
     }   
     //this comment is added from github
     public static void main(String[] args) {
-        BuddyInfo buddy = new BuddyInfo("Tommy", "Carleton", 5067891115L);
+        BuddyInfo buddy1 = new BuddyInfo("Tommy", "Carleton", 5067891115L);
+        BuddyInfo buddy2 = new BuddyInfo("Charles", "Carleton", 5067891167L);//added in Lab3-branch
         AdressBook adressBook = new AdressBook();
-        adressBook.addBuddy(buddy);
+        adressBook.addBuddy(buddy1);
+        adressBook.addBuddy(buddy2);
         adressBook.removeBuddy(4);//tommy
     }
 }
